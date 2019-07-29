3baac102b10db633a5b9091dad867b8553debb0d
christopherwburke <christopher.w.burke@gmail.com>
2019-07-29 18:05:56 -0400

    Installed Events-API

--- yodabot/tutorial.js
+++ yodabot/tutorial.js
@@ -4,3 +4,15 @@ const web = new WebClient(process.env.SLACK_TOKEN);
 
 const currentTime = new Date().toTimeString();
 
+(async () => {
+    const res = await web.auth.test()
+
+    const userId = res.user_id
+
+    await web.chat.postMessage({
+        channel: 'general',
+        text: `The current time is ${currentTime}`,
+    });
+
+    console.log('Message posted!');
+})();
