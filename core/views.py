85b05f4d03682fffd8e3e40843816808d6cd417a
mattspeidel <mattspeidel@gmail.com>
2019-08-10 15:52:12 -0400

    more attempts

--- core/views.py
+++ core/views.py
@@ -75,11 +75,11 @@ def edit_registration(request, pk):
 def registration_success(request):
     return render(request, 'registration_success.html')
 
-# def homepage(request):
-#     device = get_object_or_404(Device)
-#     return render(request, 'homepage.html', {'device': device})
 def homepage(request):
-    return render(request, 'homepage.html')
+    device = get_object_or_404(Device)
+    return render(request, 'homepage.html', {'device': device})
+# def homepage(request):
+#     return render(request, 'homepage.html')
 
 def help_page(request):
     return render(request, 'help.html')
